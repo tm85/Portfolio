b9cf50ac89124b335f2c7a0ae684e110d7e59113
tm85 <aznpunisher@gmail.com>
2017-02-23 18:21:57 -0500

    Updated Images and files
    Updated Images and files

--- script/script.js
+++ script/script.js
@@ -78,9 +78,28 @@ $(document).ready(function () {
             }).appendTo('#chooseMove');
         }//End for   
     });//End of #startGame 
-    $("#workExamples img", this).on('click',function() {
-       alert($(this).attr('title'));
-       //Open full size image when it is clicked on
-       
-    });
-  })//End of (document).ready
+    $("#workExamples img").on('click', this, function () {
+        console.log($(this).attr('title'));
+        console.log($(this).data("largeImg"));
+        //Open full size image when it is clicked onclick
+        //Get name of original image from data
+        var popUpImg = "images/" + $(this).data("largeImg");
+        $(this).after($("<div></div>")
+            .attr("id", "popUpOverlay")
+            .hide() //Hide created div to prevent it from appearing right away
+            .append(
+            $("<img></img>").attr({
+                "src": popUpImg,
+                "alt": $(this).attr('title'),
+                "title": $(this).attr('title'),
+            }))
+            .fadeIn(600));
+    });//End of #workExamples img onclick
+    $("#workExamples").on('click', "#popUpOverlay img, #popUpOverlay", function () {
+        //Fade out overlay div then remove once finished
+        $("#popUpOverlay").fadeOut(600, function () {
+            $("#popUpOverlay").remove();
+        });
+    })//End of onclick
+
+})//End of (document).ready
